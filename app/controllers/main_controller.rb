ece21e8afb90f4385fdc9e19184fe60a8f172789
Vadim Shevchenko <shev.vadim.net@gmail.com>
2013-01-10 21:48:08 +0200

    Added migration

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,8 +7,14 @@ class MainController < ApplicationController
   def create
     respond_to do |format|
       format.json do
+        urls = params[:urls]
+        result = []
+        urls.each do |url|
+          url_instance = Url.new(:url => url)
+          result.push({:url => url, :saved => true}) if url_instance.save
+        end
         response.headers['Content-type'] = "text/plain; charset=utf-8"
-        render :text => params[:urls].to_json
+        render :text => result.to_json
       end
     end
   end
